045b006cc64ea18a9953739cbf3a0c7aafa05a6a
chenk keinan <chen.keinan@appsflyer.com>
2021-03-14 10:03:09 +0200

    support loading ebpf files from folder

--- cmd/bootstrapper.go
+++ cmd/bootstrapper.go
@@ -2,9 +2,9 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/chen-keinan/kube-knark/internal/common"
 	"github.com/chen-keinan/kube-knark/internal/startup"
 	"github.com/chen-keinan/kube-knark/internal/trace"
+	"github.com/chen-keinan/kube-knark/pkg/utils"
 	"github.com/dropbox/goebpf"
 	"log"
 	"os"
@@ -30,26 +30,34 @@ func StartKnark() {
 	}
 
 	// load ebpf program
-	p, err := trace.LoadProgram(common.KprobeFile)
-	if err != nil {
-		panic("failed to load ebpf program")
+	files, err := utils.GetEbpfFiles(utils.GetEbpfCompiledFolder())
+	if err := goebpf.CleanupProbes(); err != nil {
+		log.Println(err)
 	}
-	p.ShowInfo()
+	for _, ebpfFile := range files {
+		go func() {
+			p, err := trace.LoadProgram(ebpfFile.Name)
+			if err != nil {
+				panic("failed to load ebpf program")
+			}
+			p.ShowInfo()
 
-	// attach ebpf kprobes
-	if err := p.AttachProbes(); err != nil {
-		log.Fatalf("AttachProbes() failed: %v", err)
+			// attach ebpf kprobes
+			if err := p.AttachProbes(); err != nil {
+				log.Fatalf("AttachProbes() failed: %v", err)
+			}
+			defer p.DetachProbes()
+
+			// wait until Ctrl+C pressed
+			ctrlC := make(chan os.Signal, 1)
+			signal.Notify(ctrlC, os.Interrupt)
+			<-ctrlC
+
+			// display some stats
+			fmt.Println()
+			fmt.Printf("%d Event(s) Received\n", p)
+			fmt.Printf("%d Event(s) lost (e.g. small buffer, delays in processing)\n", p.EventsLost())
+			fmt.Printf("%d Event(s) lost (e.g. small buffer, delays in processing)\n", p.EventsLost())
+		}()
 	}
-	defer p.DetachProbes()
-
-	// wait until Ctrl+C pressed
-	ctrlC := make(chan os.Signal, 1)
-	signal.Notify(ctrlC, os.Interrupt)
-	<-ctrlC
-
-	// display some stats
-	fmt.Println()
-	fmt.Printf("%d Event(s) Received\n", p)
-	fmt.Printf("%d Event(s) lost (e.g. small buffer, delays in processing)\n", p.EventsLost())
-	fmt.Printf("%d Event(s) lost (e.g. small buffer, delays in processing)\n", p.EventsLost())
 }
