701be67a5ef2d26ac0a5b6989e4b8ea4611f70ed
chenk keinan <chen.keinan@appsflyer.com>
2021-03-24 11:43:26 +0200

    Add test

--- internal/routes/spec_test.go
+++ internal/routes/spec_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/chen-keinan/kube-knark/internal/common"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -12,9 +13,9 @@ import (
 func TestRoutes(t *testing.T) {
 	f, err := os.Open(fmt.Sprintf("../fixtures/%s", common.Workload))
 	assert.NoError(t, err)
+	defer f.Close()
 	data, err := ioutil.ReadAll(f)
 	assert.NoError(t, err)
-
 	routes, err := BuildSpecRoutes([]string{string(data)})
 	if err != nil {
 		assert.NoError(t, err)
@@ -22,4 +23,17 @@ func TestRoutes(t *testing.T) {
 	assert.Equal(t, routes[0][0].Method, common.POST)
 	assert.Equal(t, routes[0][1].Method, common.PUT)
 
+}
+func TestCreateMapFromSpecFiles(t *testing.T) {
+	f, err := os.Open(fmt.Sprintf("../fixtures/%s", common.Workload))
+	assert.NoError(t, err)
+	data, err := ioutil.ReadAll(f)
+	assert.NoError(t, err)
+	spec := Spec{}
+	yaml.Unmarshal(data, &spec)
+	mapAPI, err := CreateMapFromSpecFiles([]string{string(data)})
+	assert.NoError(t, err)
+	api := spec.Categories[0].SubCategory.API[0]
+	assert.Equal(t, mapAPI[fmt.Sprintf("%s_%s", api.Method, api.URI)].Method, api.Method)
+	assert.Equal(t, mapAPI[fmt.Sprintf("%s_%s", api.Method, api.URI)].URI, api.URI)
 }
