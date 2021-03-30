76d7eb27d0635d2d0ce83db2a38c14b2ee6fcff3
chenk keinan <chen.keinan@appsflyer.com>
2021-03-30 21:36:19 +0300

    Add tests

---
+++ internal/tracer/khttp/tcp_assembly_test.go
@@ -0,0 +1,38 @@
+package khttp
+
+import (
+	"github.com/google/gopacket/layers"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestReceiveWindow(t *testing.T) {
+
+	window := newReceiveWindow(4)
+
+	// init insert
+	window.insert(&layers.TCP{Seq: 10005, BaseLayer: layers.BaseLayer{Payload: []byte{1, 2}}})
+	window.insert(&layers.TCP{Seq: 10000, BaseLayer: layers.BaseLayer{Payload: []byte{7, 8, 9, 0}}})
+	window.insert(&layers.TCP{Seq: 10010, BaseLayer: layers.BaseLayer{Payload: []byte{2, 3, 4, 5}}})
+	window.insert(&layers.TCP{Seq: 10005, BaseLayer: layers.BaseLayer{Payload: []byte{1, 2}}})
+	assert.Equal(t, 3, window.size)
+	assert.Equal(t, 0, window.start)
+	assert.Equal(t, uint32(10000), window.buffer[0].Seq)
+	assert.Equal(t, uint32(10005), window.buffer[1].Seq)
+	assert.Equal(t, uint32(10010), window.buffer[2].Seq)
+
+	window.insert(&layers.TCP{Seq: 10009, BaseLayer: layers.BaseLayer{Payload: []byte{7, 8, 9, 0}}})
+	assert.Equal(t, uint32(10000), window.buffer[0].Seq)
+	assert.Equal(t, uint32(10005), window.buffer[1].Seq)
+
+	// expand
+	window.insert(&layers.TCP{Seq: 10030, BaseLayer: layers.BaseLayer{Payload: []byte{7, 8, 9, 0}}})
+	assert.Equal(t, 5, window.size)
+	assert.Equal(t, 0, window.start)
+
+	c := make(chan *layers.TCP, 1000)
+	// confirm
+	window.confirm(10020, c)
+	assert.Equal(t, 1, window.size)
+	assert.Equal(t, 4, window.start)
+}
---
+++ internal/tracer/khttp/text_utils_test.go
@@ -0,0 +1,44 @@
+package khttp
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseMimeType(t *testing.T) {
+	var mimeType = parseMimeType("application/json")
+	assert.Equal(t, "application", mimeType.Type)
+	assert.Equal(t, "json", mimeType.subType)
+	assert.Equal(t, "", mimeType.scope)
+	assert.True(t, mimeType.isTextContent())
+	assert.False(t, mimeType.isBinaryContent())
+}
+
+func TestParseXMimeType(t *testing.T) {
+	var mimeType = parseMimeType("application/x-msdownload")
+	assert.Equal(t, "application", mimeType.Type)
+	assert.Equal(t, "msdownload", mimeType.subType)
+	assert.Equal(t, "x", mimeType.scope)
+	assert.False(t, mimeType.isTextContent())
+	assert.True(t, mimeType.isBinaryContent())
+}
+
+func TestVndMimeType(t *testing.T) {
+	var mimeType = parseMimeType("application/vnd.ms-powerpoint.template.macroenabled.12")
+	assert.Equal(t, "application", mimeType.Type)
+	assert.Equal(t, "ms-powerpoint", mimeType.subType)
+	assert.Equal(t, "vnd", mimeType.scope)
+	assert.False(t, mimeType.isTextContent())
+	assert.True(t, mimeType.isBinaryContent())
+}
+
+func TestWildcardMatch(t *testing.T) {
+	assert.True(t, wildcardMatch("test", "test"))
+	assert.True(t, wildcardMatch("test", "tes*"))
+	assert.True(t, wildcardMatch("test", "tes?"))
+	assert.True(t, wildcardMatch("test", "t*"))
+	assert.True(t, wildcardMatch("test", "*t*"))
+	assert.False(t, wildcardMatch("test", "tt*"))
+	assert.False(t, wildcardMatch("test", "es"))
+}
